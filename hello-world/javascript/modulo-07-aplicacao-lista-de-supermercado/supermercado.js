1f75e1a07c52c79b5a214c2bc64212375779505a
Guilherme Sampaio <46483050+sampamito@users.noreply.github.com>
2021-12-13 21:26:21 -0300

    Finalizado o módulo 07 - App Lista de Supermercado
    Finalizado o módulo 07 - App Lista de Supermercado, com a finalização da lista, com os clicks de cadastro dinamico e limpeza da lista.

--- hello-world/javascript/modulo-07-aplicacao-lista-de-supermercado/supermercado.js
+++ hello-world/javascript/modulo-07-aplicacao-lista-de-supermercado/supermercado.js
@@ -1,13 +1,42 @@
 var items = [];
 
-document.querySelector('input[type=submit]').addEventListener('click',()=>{
-    var nomeProduto = document.querySelector('input[name=nome_produto]').value;
-    var valorProduto = document.querySelector('input[name=valor_produto]').value;
+document.querySelector('input[type=submit]').addEventListener('click', () => {
+    var nomeProduto = document.querySelector('input[name=nome_produto]');
+    var valorProduto = document.querySelector('input[name=valor_produto]');
 
     items.push({
-        nome: nomeProduto,
-        valor: valorProduto
+        nome: nomeProduto.value,
+        valor: valorProduto.value
     });
 
-    alert(items[0].nome);
-})
+    let listaProdutos = document.querySelector('.lista-produtos');
+    let soma = 0;
+    listaProdutos.innerHTML = "";
+    items.map(function (val) {
+        soma+=parseFloat(val.valor);
+
+        listaProdutos.innerHTML += `
+        <div class="lista-produto-single">
+        <h3>${val.nome}</h3>
+        <h3 class="price-produto"><span>R$${val.valor}</span></h3>
+        </div>  
+        
+        `;
+
+    })
+
+    soma = soma.toFixed(2);
+    nomeProduto.value = "";
+    valorProduto.value = "";
+
+    let elementoSoma = document.querySelector('.soma-produto h1');
+    elementoSoma.innerHTML = "Total: R$ " + soma;
+    
+});
+
+document.querySelector('button[name=limpar').addEventListener('click',()=>{
+    items = [];
+
+    document.querySelector('.lista-produtos').innerHTML = "";
+    document.querySelector('.soma-produto h1').innerHTML = "Total: R$ ";
+});
