c0f405dfd8d5c8c1f902ed157e589dd527b20d03
Guilherme Sampaio <46483050+sampamito@users.noreply.github.com>
2021-12-15 08:55:50 -0300

    Conclusao do Modulo 09
    Conclusao do Modulo 09 - App Pokedex com integracao com API

--- hello-world/javascript/modulo09-pokedex-api/pokedex.js
+++ hello-world/javascript/modulo09-pokedex-api/pokedex.js
@@ -1,3 +1,55 @@
-fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-    .then(response => response.json())
-    .then(allpokemon => console.log(allpokemon))
+
+/**
+ * tratamento para nao executar a funcao antes de carregar
+ * toda a DOM totalmente
+ */
+window.onload = () => {
+    var quantity = document.getElementById("quantity");
+    quantity.addEventListener("keyup", () => {
+        getPokemons(quantity.value);
+    });
+}
+
+/**
+ * funcao getPokemons
+ * @param {quantidade de pokemons a buscar na api} number 
+ */
+function getPokemons(number) {
+
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=' + number)
+        .then(response => response.json())
+        .then(allpokemon => {
+
+            var pokemons = [];
+
+            allpokemon.results.map((val) => {
+
+                fetch(val.url)
+                    .then(response => response.json())
+                    .then(pokemonSingle => {
+
+                        pokemons.push({ name: val.name, image: pokemonSingle.sprites.front_default });
+
+                        if (pokemons.length == number) {
+
+                            var pokemonBoxes = document.querySelector('.pokemon-boxes');
+                            pokemonBoxes.innerHTML = "";
+
+                            pokemons.map((val) => {
+                                pokemonBoxes.innerHTML += `
+                            <div class="pokemon-box">
+                                <img src="${val.image}" />
+                                <p>${val.name}</p>
+                                </div>
+                            `;
+
+                            })
+
+                        }
+
+                    })
+
+            })
+
+        })
+}
